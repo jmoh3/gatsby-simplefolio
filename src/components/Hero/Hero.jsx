6a7f2272bff2f89749108bc061f0168f2a412a86
jmoh3 <jmoh3@illinois.edu>
2020-06-21 01:02:46 -0500

    Fix mobile formatting issues

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,7 +22,7 @@ const Header = () => {
   }, []);
 
   return (
-    <section id="hero" className="jumbotron">
+    <section id="hero" className="jumbotron top">
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
